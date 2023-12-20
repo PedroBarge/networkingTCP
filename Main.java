48f3eecb1d0657c92f1971f8252fd2d1d54f3675
PedroBarge <pedro525tavares@gmail.com>
2023-12-20 09:43:12 +0000

    add title

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        System.out.println("Server TCP");
+        System.out.println("Server TCP start");
         Server server = new Server();
         server.start(6666);
     }
